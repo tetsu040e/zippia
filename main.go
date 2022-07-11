4ea2a83be52d770c8f29bb49fc3c872cc0869790
Toru SASAKI <tetsu@moove.co.jp>
2022-07-11 22:35:11 +0900

    refactoring

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		zip := r.URL.Query().Get("zip")
 
-		stmt, err := db.Prepare("SELECT zip, pref, city, town, pref_kana, city_kana, town_kana FROM row WHERE zip = ?")
+		stmt, err := db.Prepare("SELECT zip, pref, city, town, pref_kana, city_kana, town_kana FROM address WHERE zip = ?")
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -32,14 +32,14 @@ func main() {
 			log.Fatal(err)
 		}
 		defer rows.Close()
-		list := []Row{}
+		list := []Address{}
 		for rows.Next() {
-			var row Row
-			err = rows.Scan(&row.Zip, &row.Pref, &row.City, &row.Town, &row.PrefKana, &row.CityKana, &row.TownKana)
+			var address Address
+			err = rows.Scan(&address.Zip, &address.Pref, &address.City, &address.Town, &address.PrefKana, &address.CityKana, &address.TownKana)
 			if err != nil {
 				break
 			}
-			list = append(list, row)
+			list = append(list, address)
 		}
 
 		body, err := json.Marshal(list)
@@ -50,14 +50,14 @@ func main() {
 		fmt.Fprintf(w, string(body))
 	})
 
-	fmt.Println("http server started on localhost:8080")
+	log.Println("http server started on localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
 
 //go:embed kenall.json
 var kenall []byte
 
-type Row struct {
+type Address struct {
 	Id       int    `json:"-"`
 	Zip      string `json:"zip"`
 	Pref     string `json:"pref"`
@@ -69,41 +69,41 @@ type Row struct {
 }
 
 func initialize() (*sql.DB, error) {
-	fmt.Println("initializing...")
+	log.Println("initializing DB...")
 	db, err := sql.Open("sqlite3", "file:zip.db?cache=shared&mode=memory")
 	if err != nil {
 		return nil, err
 	}
 
 	initialTableSQL := `
-    DROP INDEX IF EXISTS zipcode;
-    DROP TABLE IF EXISTS smaple;
-    CREATE TABLE IF NOT EXISTS row (
-        id        INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        zip      TEXT,
-        pref      TEXT,
-        city      TEXT,
-        town      TEXT,
-        pref_kana TEXT,
-        city_kana TEXT,
-        town_kana TEXT
-    );
-    CREATE INDEX zipcode ON row (zip);
+DROP INDEX IF EXISTS zipcode;
+DROP TABLE IF EXISTS address;
+CREATE TABLE IF NOT EXISTS address (
+    id        INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    zip       TEXT,
+    pref      TEXT,
+    city      TEXT,
+    town      TEXT,
+    pref_kana TEXT,
+    city_kana TEXT,
+    town_kana TEXT
+);
+CREATE INDEX zipcode ON address (zip);
     `
 	_, err = db.Exec(initialTableSQL)
 	if err != nil {
 		return nil, err
 	}
 
-	var zipList []Row
-	if err := json.Unmarshal(kenall, &zipList); err != nil {
+	var list []Address
+	if err := json.Unmarshal(kenall, &list); err != nil {
 		return nil, err
 	}
 
-	buf := []Row{}
-	bufSize := 50
-	for _, row := range zipList {
-		buf = append(buf, row)
+	const bufSize = 100
+	buf := []Address{}
+	for _, address := range list {
+		buf = append(buf, address)
 		if len(buf) >= bufSize {
 			err = flush(db, &buf)
 			if err != nil {
@@ -119,13 +119,13 @@ func initialize() (*sql.DB, error) {
 	}
 
 	db.SetConnMaxLifetime(-1)
-	fmt.Println("initialized is completed!!")
+	log.Println("DB initialization is completed!")
 	return db, nil
 }
 
-func flush(db *sql.DB, buf *[]Row) error {
+func flush(db *sql.DB, buf *[]Address) error {
 	insertSQL, args := createBulkInsertQuery(*buf)
-	*buf = []Row{}
+	*buf = []Address{}
 
 	stmt, err := db.Prepare(insertSQL)
 	if err != nil {
@@ -140,16 +140,16 @@ func flush(db *sql.DB, buf *[]Row) error {
 	return nil
 }
 
-func createBulkInsertQuery(list []Row) (string, []interface{}) {
+func createBulkInsertQuery(list []Address) (string, []interface{}) {
 	placeholder := []string{}
 	args := []interface{}{}
 
-	for _, row := range list {
+	for _, address := range list {
 		placeholder = append(placeholder, "(?,?,?,?,?,?,?)")
-		args = append(args, row.Zip, row.Pref, row.City, row.Town, row.PrefKana, row.CityKana, row.TownKana)
+		args = append(args, address.Zip, address.Pref, address.City, address.Town, address.PrefKana, address.CityKana, address.TownKana)
 	}
 
-	query := fmt.Sprintf("INSERT INTO row (zip, pref, city, town, pref_kana, city_kana, town_kana) VALUES %s", strings.Join(placeholder, ","))
+	query := fmt.Sprintf("INSERT INTO address (zip, pref, city, town, pref_kana, city_kana, town_kana) VALUES %s", strings.Join(placeholder, ","))
 
 	return query, args
 }
