36ebbc720db58347926a6a5da8d4569f1b9724a1
Toru SASAKI <tetsu@moove.co.jp>
2022-07-19 17:24:55 +0900

    add check for update script

---
+++ scripts/check_for_updates.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+url="https://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip"
+last_modified_path=$(dirname $0)/../var/last-modified.txt
+last_modified=$(cat ${last_modified_path})
+
+status_code=`curl $url -I -H "If-Modified-Since: ${last_modified}" -w %{http_code} -o /dev/null -s`
+if [ "$status_code" -eq "200" ]; then
+    curl $url -I -s | grep "^Last-Modified" | cut -f2- -d' ' > $last_modified_path
+    printf "updated"
+fi
+
+exit 0
