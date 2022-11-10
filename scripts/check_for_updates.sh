9154ea07f7cd45527786f93729c3befe6a48b97b
Toru SASAKI <tetsu@moove.co.jp>
2022-11-10 12:08:18 +0900

    :wrench: deprecated set-output

--- scripts/check_for_updates.sh
+++ scripts/check_for_updates.sh
@@ -7,7 +7,7 @@ last_modified=$(cat ${last_modified_path})
 status_code=`curl $url -I -H "If-Modified-Since: ${last_modified}" -w %{http_code} -o /dev/null -s`
 if [ "$status_code" -eq "200" ]; then
     curl $url -I -s | grep "^Last-Modified" | cut -f2- -d' ' | tr -d "\r\n" > $last_modified_path
-    echo "::set-output name=updated::true"
+    echo "updated=true" >> $GITHUB_OUTPUT
 fi
 
 exit 0
